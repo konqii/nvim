9f3eb37578d6a28014416d200a6124eb9de69c44
Mike Wanninger <mikewanninger@lufthai.localdomain>
2024-03-21 14:46:04 +0100

    add highlight autocommand for yanking text

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -82,3 +82,11 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
